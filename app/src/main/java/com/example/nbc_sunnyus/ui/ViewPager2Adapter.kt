cd8289983099b298e80b85fcca6dcce0dde940d8
hyejeong <0601a@naver.com>
2024-04-25 10:45:58 +0900

    [feat/#3] companion object 수정

--- app/src/main/java/com/example/nbc_sunnyus/ui/ViewPager2Adapter.kt
+++ app/src/main/java/com/example/nbc_sunnyus/ui/ViewPager2Adapter.kt
@@ -5,23 +5,21 @@ import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-object TabNumbers {
-    const val NUM_TABS = 2     //key 값 (사용법 : TabNumbers.NUM_TABS 로 접근 가능)
-}
-
-
 class ViewPager2Adapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
     FragmentStateAdapter(fragmentManager, lifecycle) {
 
     //페이지 개수
-    override fun getItemCount(): Int = TabNumbers.NUM_TABS
+    override fun getItemCount(): Int = NUM_TABS
 
     //파라미터: position값, 반환값: Fragment
     override fun createFragment(position: Int): Fragment {
         return when (position) {
-            0 -> return ContactListFragment()
-            else -> return MyPageFragment()
+            0 -> ContactListFragment()
+            else -> MyPageFragment()
         }
     }
 
+    companion object {
+        private const val NUM_TABS = 2     //key 값 (사용법 : TabNumbers.NUM_TABS 로 접근 가능)
+    }
 }
